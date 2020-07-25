45e2f92868c1d7d0dfd0bf4faeba5476341241a9
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-07-25 18:36:56 +0900

    dd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -369,6 +369,7 @@ class Controls:
     if self.enabled != self.controlsAllowed and self.controlsAllowed:
       if CS.vEgo > 1.0 and CS.gearShifter == 2:
         self.enabled = True
+        self.active = True
     
     print( 'enable={} active={} controlsAllowed={}'.format( self.enabled, self.active, self.controlsAllowed ) )
 
